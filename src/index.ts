f52c451600f87bec067bb9240c32c2f5a7211688
anaritamed <ana.rita.medeiros.souza@ccc.ufcg.edu.br>
2026-01-08 23:43:48 -0300

    chore: adiciona handle de erros

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
 import { routes } from './routes/routes';
+import { AppError } from './errors/AppError';
 
 const app = express();
 const port = process.env.PORT ?? 3333;
@@ -11,3 +13,23 @@ app.use('/api', routes);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  if (err instanceof ZodError) {
+    const formattedErrors = err.issues.map(error => ({
+      message: `${error.path.join('.')}: ${error.message}`,
+    }));
+    res.status(400).json({ errors: formattedErrors });
+    return;
+  }
+
+  if (err instanceof AppError) {
+    res.status(err.statusCode).json({ message: err.message });
+    return;
+  }
+
+  console.error(err);
+  res.status(500).json({ message: 'Erro do servidor' });
+};
+
+app.use(errorHandler);
