5c24f6318b4052d12728b8fd36446e12181f861e
Naiara Medeiros <naiara.medeiros.costa.almeida@ccc.ufcg.edu.br>
2026-01-15 21:39:38 -0300

    feat: cria pasta de testes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { recrutadorRoutes } from './recrutador.routes';
 import { candidatoRoutes } from './candidato.routes';
 import { perfilRoutes } from './perfil.routes';
-import { vagaRoutes } from './vagas.routes';
+import { vagaRoutes } from './vaga.routes';
 
 export const routes = Router();
 
