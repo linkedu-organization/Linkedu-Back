637595813b529e72de8e6e53633b3dc0bfed9c50
Naiara Medeiros <naiara.medeiros.costa.almeida@ccc.ufcg.edu.br>
2026-01-26 22:30:06 -0300

    chore: cria schema para recomendacao

---
+++ src/models/RecomendacaoSchema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const RecomendacaoSchema = z.object({
+  vagaId: z.number(),
+  candidatoId: z.number(),
+  updatedAt: z.date().or(z.iso.datetime()),
+  descricao: z.string(),
+});
+
+export const RecomendacaoCreateSchema = RecomendacaoSchema;
+
+export const RecomendacaoResponseSchema = RecomendacaoSchema.extend({
+  id: z.number(),
+});
+
+export type RecomendacaoCreateDTO = z.infer<typeof RecomendacaoCreateSchema>;
+export type RecomendacaoResponseDTO = z.infer<typeof RecomendacaoResponseSchema>;
---
+++ src/repositories/RecomendacaoRepository.ts
@@ -0,0 +1,8 @@
+
+import { RecomendacaoCreateDTO } from '../models/RecomendacaoSchema';
+
+class RecomendacaoRepository {
+  async create(data: RecomendacaoCreateDTO) {
+    // criar uma nova recomendação com IA e dados fornecidos
+  }
+}
