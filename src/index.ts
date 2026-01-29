b83aa3c267543505967855f147c5e4436768e806
Naiara Medeiros <naiara.medeiros.costa.almeida@ccc.ufcg.edu.br>
2026-01-29 19:19:39 -0300

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { ErrorRequestHandler } from 'express';
 import { ZodError } from 'zod';
+import cors from 'cors';
 
 import { routes } from './routes/routes';
 import { AppError } from './errors/AppError';
@@ -7,6 +8,12 @@ import { AppError } from './errors/AppError';
 const app = express();
 const port = process.env.PORT ?? 3333;
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api', routes);
 
