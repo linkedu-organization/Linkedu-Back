65e03ddd7bc2f1f43bc2f0f977ebe78e95a1745f
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2026-02-28 15:58:31 -0300

    fix: erro de CORS

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,18 @@ import { AppError } from './errors/AppError';
 
 const app = express();
 const port = process.env.PORT ?? 3333;
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 app.disable('x-powered-by');
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(cookieParser());
 app.use('/api', routes);
 
