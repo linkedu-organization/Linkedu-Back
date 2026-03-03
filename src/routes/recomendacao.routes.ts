1f6f324a26b953e841d23c505f1ed32df409bcc6
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2026-03-03 11:36:43 -0300

    chore: altera endpoint

--- src/routes/recomendacao.routes.ts
+++ src/routes/recomendacao.routes.ts
@@ -5,18 +5,18 @@ import { ensureIsCandidato, ensureIsRecrutador } from '../middlewares/authMiddle
 
 export const recomendacaoRoutes = Router();
 
-recomendacaoRoutes.post('/vaga/', ensureIsCandidato, async (req, res) => {
+recomendacaoRoutes.post('/vagas-para-candidato/', ensureIsCandidato, async (req, res) => {
   await recomendacaoController.createRecomendacaoVagasParaCandidato(req, res);
 });
 
-recomendacaoRoutes.post('/candidato/:vagaId', ensureIsRecrutador, async (req, res) => {
+recomendacaoRoutes.post('/candidatos-para-vaga/:vagaId', ensureIsRecrutador, async (req, res) => {
   await recomendacaoController.createRecomendacaoCandidatosParaVaga(req, res);
 });
 
-recomendacaoRoutes.get('/vaga/', ensureIsCandidato, async (req, res) => {
+recomendacaoRoutes.get('/vagas-para-candidato/', ensureIsCandidato, async (req, res) => {
   await recomendacaoController.getRecomendacaoVagasParaCandidato(req, res);
 });
 
-recomendacaoRoutes.get('/candidato/:vagaId', ensureIsRecrutador, async (req, res) => {
+recomendacaoRoutes.get('/candidatos-para-vaga/:vagaId', ensureIsRecrutador, async (req, res) => {
   await recomendacaoController.getRecomendacaoCandidatosParaVaga(req, res);
 });
